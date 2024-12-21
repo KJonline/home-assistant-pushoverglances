86c9bfb641625a18655d6776023e062d87442477
Khole <khole@gmail.com>
2024-12-21 19:52:07 +0000

    fix const file

--- custom_components/pushoverglances/const.py
+++ custom_components/pushoverglances/const.py
@@ -1,18 +1,17 @@
 """Constants for Push Over Glances."""
 
 from typing import Final
+from homeassistant.const import Platform
 
 DOMAIN = "pushoverglances"
-PLATFORMS = [
-    Platform.NOTIFY
-]
+PLATFORMS = [Platform.NOTIFY]
 
 CONFIG_ENTRY_VERSION = 1
 DATA_HASS_CONFIG: Final = "pushover_hass_config"
 DEFAULT_NAME: Final = "Pushover Glances"
 
-ATTR_COUNT = 'count'
-ATTR_PERCENT = 'percent'
-ATTR_SUBTEXT = 'subtext'
+ATTR_COUNT = "count"
+ATTR_PERCENT = "percent"
+ATTR_SUBTEXT = "subtext"
 
 CONF_USER_KEY: Final = "user_key"
